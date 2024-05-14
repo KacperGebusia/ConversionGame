85229c2f5120f826f14fe9f865e31f4ae1abb47e
KacperGebusia <135381595+KacperGebusia@users.noreply.github.com>
2024-05-14 19:43:53 +0200

    feat: number draw

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('game_canvas');
 const ctx = canvas.getContext('2d');
 
+
+let number_dec = Math.floor(Math.random()*255)+1;
+
 const cursor = {
     x: 0,
     y: 0
@@ -15,7 +18,25 @@ canvas.addEventListener('click', function(e) {
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
 
+    if (cursor.x >= 1030 && cursor.x <= 1150 && cursor.y >= 730 && cursor.y <= 780){
+        number_dec = Math.floor(Math.random()*255)+1;
+        showUI();
+    }
+
+
     console.log(cursor);
 });
 
+function showUI(){
+    ctx.font = "42px Arial";
+    ctx.fillStyle = "#FFD38D";
+    ctx.fillText("Liczba dziesiętna do przedstawienia binarnego: " + number_dec, 30, 770);
 
+    ctx.fillStyle = "brown";
+    ctx.fillRect(1030, 730, 120, 50);
+    ctx.font = "28px Arial";
+    ctx.fillStyle = "#FFD38D";
+    ctx.fillText("LOSUJ", 1045, 765);
+}
+
+showUI();
