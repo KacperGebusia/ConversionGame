97e2a711e7f4d67bcb2b9d055044b2244ffe3d39
KacperGebusia <135381595+KacperGebusia@users.noreply.github.com>
2024-05-14 19:28:16 +0200

    feat: click handling

--- game.js
+++ game.js
@@ -1,3 +1,18 @@
 const canvas = document.getElementById('game_canvas');
 const ctx = canvas.getContext('2d');
 
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+canvas.addEventListener('click', function(e) {
+
+    let game_rect = canvas.getBoundingClientRect();
+    cursor.x = e.clientX - game_rect.left;
+    cursor.y = e.clientY - game_rect.top;
+
+    console.log(cursor.x, cursor.y);
+});
+
+
